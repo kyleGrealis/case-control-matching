200bedeb69bdf6ad8e82b3f8c1a60cf6927fe76f
Kyle Grealis <kyleGrealis@icloud.com>
2023-12-17 10:00:35 -0600

    edit: adding new data, info, and inputs modules

--- app/main.R
+++ app/main.R
@@ -1,12 +1,12 @@
 box::use(
-  bsicons[bs_icon],
-  bslib[page_fixed, tooltip],
-  glue[glue],
-  rio[import],
-  shiny[actionButton, conditionalPanel, div, fileInput, mainPanel, moduleServer,
-        NS, numericInput, selectInput, sidebarLayout, sidebarPanel, reactive,
-        req, renderTable, renderText, renderUI, tableOutput, tags, uiOutput],
-  utils[head]
+  bslib[page_fixed],
+  shiny[mainPanel, moduleServer, NS, sidebarLayout, sidebarPanel],
+)
+
+box::use(
+  app/view/data,
+  app/view/data_info,
+  app/view/inputs,
 )
 
 #' @export
@@ -14,24 +14,14 @@ ui <- function(id) {
   ns <- NS(id)
   page_fixed(
     sidebarLayout(
-
       sidebarPanel(
-        fileInput(ns("file1"), "Choose any file", accept = NULL),
-        uiOutput(ns("idVariable")),
-        uiOutput(ns("caseControl")),
-        uiOutput(ns("numericVariable")),
-        uiOutput(ns("numRangeCat")),
-        uiOutput(ns("thirdVariable")),
-        uiOutput(ns("matchButton"))
-      ), # sidebarPanel
-
+        data$ui(ns("file1")),
+        inputs$ui(ns("inputs"))
+      ),
       mainPanel(
-        uiOutput(ns("tableSummaryMessage")),
-        tableOutput(ns("contents")),
-        uiOutput(ns("tableFootnote"))
-      ) # mainPanel
-
-    ) # sidebarLayout
+        data_info$ui(ns("info")),
+      )
+    )
   )
 }
 
@@ -39,166 +29,8 @@ ui <- function(id) {
 server <- function(id) {
   moduleServer(id, function(input, output, session) {
     ns <- NS(id)
-    file <- reactive(input$file1)
-    newFile <- reactive({
-      if (is.null(file())) {
-        return(NULL)
-      }
-      rio::import(file()$datapath)
-    })
-
-    output$contents <- renderTable({
-      if (is.null(file())) {
-        return(NULL)
-      }
-      head(newFile(), n = 20)
-    })
-
-    output$tableSummaryMessage <- renderUI({
-      if (is.null(file())) {
-        return(NULL)
-      }
-      glue::glue(
-        "The table has {nrow(newFile())} rows and {ncol(newFile())} columns."
-
-      )
-    })
-
-    output$tableFootnote <- renderUI({
-      if (is.null(file())) {
-        return(NULL)
-      }
-      glue::glue(
-        "Displaying only the first 20 rows."
-      )
-    })
-
-    output$idVariable <- renderUI({
-      if (is.null(file())) {
-        return(NULL)
-      }
-      list(
-        bslib::tooltip(
-          bsicons::bs_icon("info-circle", title = "About tooltips"),
-          "Text shown in the tooltip."
-        ),
-        selectInput(
-          ns("idVariable"), "Choose ID variable.",
-          choices = c("", names(newFile())),
-          selected = ""
-        )
-      )
-    })
-
-    output$caseControl <- renderUI({
-      if (is.null(file())) {
-        return(NULL)
-      }
-      conditionalPanel(
-        condition = "input.idVariable != ''",
-        # condition = 'ns("idVariable") != ""',
-        # condition = paste0('input[\'', ns('idVariable'), "\'] != ''"),
-        # condition = glue::glue("input[{ns('idVariable')}] != ''"), # works properly
-        # ns = ns,
-        selectInput(
-          ns("caseControl"), "Choose case-control variable.",
-          choices = c(
-            "",
-            setdiff(
-              newFile() |> purrr::keep(is.numeric) |> names(),
-              c(input$idVariable)
-            )
-          )
-        )
-      )
-    })
-
-    output$numericVariable <- renderUI({
-      if (is.null(file())) {
-        return(NULL)
-      }
-      conditionalPanel(
-        condition = "input.caseControl != ''",
-        selectInput(
-          ns("numericVariable"), "Choose numeric matching variable.",
-          choices = c(
-            "",
-            setdiff(
-              newFile() |> purrr::keep(is.numeric) |> names(),
-              c(
-                input$idVariable,
-                input$caseControl
-              )
-            )
-          )
-        )
-      )
-    })
-
-    output$numRangeCat <- renderUI({
-      if (is.null(file()) | is.null(input$numericVariable)) {
-        return(NULL)
-      }
-      conditionalPanel(
-        condition = "input.numericVariable != ''",
-        numericInput(
-          ns("numVarRange"), "Choose matching range of numeric variable.",
-          min = 0, max = 100, step = 1, value = 1
-        ),
-        renderText(
-          "Choosing 0 will cause exact matching. If a case has an age of 30,
-          then it will only be matched to controls that are also 30 years old.
-          If you choose 1, then the case will be matched to controls aged 29 to 31."
-        ),
-        selectInput(
-          ns("categoricalVariable"), "Choose categorical matching variable.",
-          choices = c(
-            "",
-            setdiff(
-              newFile() |> purrr::keep(is.character) |> names(),
-              c(
-                input$idVariable,
-                input$caseControl,
-                input$numericVariable
-              )
-            ) # setiff
-          ) # choices
-        )
-      ) # conditionalPanel
-    })
-
-    output$thirdVariable <- renderUI({
-      if (is.null(file())) {
-        return(NULL)
-      }
-      conditionalPanel(
-        condition = "input.categoricalVariable != ''",
-        selectInput(
-          ns("thirdVariable"), "Choose categorical matching variable.",
-          choices = c(
-            "leave blank if not needed" = "",
-            setdiff(
-              newFile() |> names(),
-              c(
-                input$idVariable,
-                input$caseControl,
-                input$numericVariable,
-                input$categoricalVariable
-              )
-            ) # setdiff
-          ) # choices
-        ) # selectInput
-      )
-    })
-
-    output$matchButton <- renderUI({
-      if (is.null(file())) {
-        return(NULL)
-      }
-      conditionalPanel(
-        condition = "input.categoricalVariable != ''",
-        actionButton("matchButton", "Match cases to controls")
-      )
-    })
-  }) # moduleServer
+    newFile <- data$server("file1")
+    data_info$server("info", newFile)
+    inputs$server("inputs", newFile)
+  })
 }

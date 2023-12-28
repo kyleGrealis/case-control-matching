e588d39466d521fdcda3a5050e6113471007b92d
Kyle Grealis <kyleGrealis@icloud.com>
2023-12-28 17:49:33 -0600

    feat: matching & iterations

--- test_algo.R
+++ test_algo.R
@@ -12,41 +12,21 @@ numVar <- "age"
 numRange <- 2
 catVar <- "gender"
 ratio <- 2
+# thirdVar <- NULL
 thirdVar <- "ethnicity"
 
-# Apply the function
-matched_data <- mitter_match(
-  dat,
-  idVar,
-  caseControl,
-  numVar,
-  numRange,
-  catVar,
-  ratio,
-  thirdVar = NULL
-)
-
-# set up the empty function
-mitter_match <- function(dat, idVar, caseControl, numVar, numRange, catVar, ratio, thirdVar = NULL) {
-  # fill in with pieces as they are working
-
-  # Convert the variable names to symbols
-  idVar <- rlang::sym(idVar)
-  caseControl <- rlang::sym(caseControl)
-  catVar <- rlang::sym(catVar)
-  numVar <- rlang::sym(numVar)
-  if (!is.null(thirdVar)) {
-    thirdVar <- rlang::sym(thirdVar)
-  }
+# Convert the variable names to symbols
+idVar <- rlang::sym(idVar)
+caseControl <- rlang::sym(caseControl)
+catVar <- rlang::sym(catVar)
+numVar <- rlang::sym(numVar)
+if (!is.null(thirdVar)) {
+  thirdVar <- rlang::sym(thirdVar)
 }
-
-
 
 # Split the data into cases and controls
 case <- dat |>
   filter({{ caseControl }} == 1) |>
-  # Add case_usage as a column
-  mutate(case_usage = 0) |>
   rename(
     case_id = {{ idVar }},
     case_num_var = {{ numVar }},
@@ -67,12 +47,18 @@ control <- dat |>
   ) |>
   select(-c({{ caseControl }}))
 
+# set this to be an initial empty value
+combined_rows <- NULL
+
+
 # Define a function to find two controls for each case and return a single row
 # that includes the case and its matched controls
-# find_controls <- function(idVar) {
-find_controls <- function(this_case) {
+find_matches <- function(this_case) {
 
-    # Get the case row
+  # test as the function is built
+  # this_case <- case[2,1]
+
+  # Get the case row
   case_row <- case |> filter(case_id == this_case)
 
   # Access the numVar and catVar columns in the case_row data frame
@@ -89,9 +75,11 @@ find_controls <- function(this_case) {
       control_num_var >= numeric_value - numRange,
       control_num_var <= numeric_value + numRange,
       control_third_var == third_value,
-      # Only consider controls that have been used once or not at all
-      control_usage <= ratio
+      # Only consider controls that have not been used
+      !control_id %in% combined_rows$control_id,
+      control_usage == 0
     )
+  # browser()
 
   # If there are no possible matches, return NULL
   if (nrow(possible_controls) == 0) {
@@ -102,231 +90,214 @@ find_controls <- function(this_case) {
   matched_controls <- possible_controls |>
     slice_sample(n = ratio)
 
-  # Update the case_usage column for the matched controls
-  case$case_usage[case$case_id %in% matched_controls$case_id] <-
-    case$case_usage[case$case_id %in% matched_controls$case_id] + 1
-
-  # Update the control_usage column for the matched controls
-  control$control_usage[control$control_id %in% matched_controls$control_id] <-
-    control$control_usage[control$control_id %in% matched_controls$control_id] + 1
-
   # Combine the case and its matched controls into a single row
-  combined_row <- bind_cols(case_row, matched_controls) |>
+  combined_rows <- bind_cols(case_row, matched_controls) |>
     select(case_id, case_num_var, case_cat_var, case_third_var, control_id)
+  # browser()
 
-  return(combined_row)
-}
+  # Update the control usage count
+  control <<- control |>
+    mutate(
+      control_usage = case_when(
+        control_id %in% combined_rows$control_id ~ 1,
+        TRUE ~ control_usage
+      )
+    )
+  # browser()
 
+  return(combined_rows)
+  # return(list(combined_rows = combined_rows, control = control))
+} # end of find_matches()
+
+
+# test find_matches() on one case
+this_case <- case[2,1]
+result <- find_matches(this_case)
+
+# test find_matches on all cases and calculate computation time
+start_time <- proc.time()
+matched_data <- case$case_id |> map_dfr(find_matches)
+end_time <- proc.time()
+end_time - start_time
 # works to this point -----------------------------------------------------
 
-# filter the cases from the `dat` dataset that have not met the `ratio`
-# number of matches
 
-  # Try to find matches for each case
-  iteration <- 0
-  while (iteration < 100) {
-    new_matches <- case$case_id |> map_dfr(find_controls)
-    # browser()
-    if (nrow(new_matches) > 0) {
-      break
-    }
-    iteration <- iteration + 1
+# build the function ------------------------------------------------------
+
+# set up the empty function
+mitter_match <- function(dat, idVar, caseControl, numVar, numRange, catVar, ratio, thirdVar = NULL) {
+
+  # Convert the variable names to symbols
+  idVar <- rlang::sym(idVar)
+  caseControl <- rlang::sym(caseControl)
+  catVar <- rlang::sym(catVar)
+  numVar <- rlang::sym(numVar)
+  if (!is.null(thirdVar)) {
+    thirdVar <- rlang::sym(thirdVar)
   }
 
-  # If no new matches were found, return the original data
-  if (nrow(new_matches) == 0) {
-    warning("No new matches found after max_iterations. Returning original data.")
-    return(dat)
-  }
-
-  # Combine the original data with the new matches
-  combined_data <- bind_rows(dat, new_matches)
-
-  return(combined_data)
-
-
-
-# testing area ------------------------------------------------------------
-
-
-
-
-
-# count the number of unique cases and controls
-matched_data |>
-  summarise(
-    n_cases = n_distinct(case_id),
-    n_controls = n_distinct(control_id)
-  )
-
-# find if any case is listed more than `ratio` times
-matched_data |>
-  group_by(case_id) |>
-  summarise(n = n()) |>
-  filter(n > ratio) |>
-  nrow()
-
-# find any cases that were not matched
-case |>
-  filter(!idVar %in% matched_data$case_id) |>
-  nrow()
-
-# find any control that were not matched
-control |>
-  filter(!idVar %in% matched_data$control_id) |>
-  nrow()
-
-# find if any cases were matched only once
-matched_data |>
-  group_by(case_id) |>
-  summarise(n = n()) |>
-  filter(n == 1) |>
-  nrow()
-
-# find if any controls were matched more than once
-matched_data |>
-  group_by(control_id) |>
-  summarise(n = n()) |>
-  filter(n > 1) |>
-  nrow()
-
-
-
-
-
-
-# original working function -----------------------------------------------
-
-
-mitter_match_original <- function(dat, numRange, ratio) {
   # Split the data into cases and controls
   case <- dat |>
-    dplyr::filter(event == 1)
+    filter({{ caseControl }} == 1) |>
+    rename(
+      case_id = {{ idVar }},
+      case_num_var = {{ numVar }},
+      case_cat_var = {{ catVar }},
+      case_third_var = {{ thirdVar }}
+    ) |>
+    select(-c({{ caseControl }}))
+
   control <- dat |>
-    dplyr::filter(event == 0) |>
-    mutate(control_usage = 0)  # Add control_usage as a column
+    filter({{ caseControl }} == 0) |>
+    # Add control_usage as a column
+    mutate(control_usage = 0) |>
+    rename(
+      control_id = {{ idVar }},
+      control_num_var = {{ numVar }},
+      control_cat_var = {{ catVar }},
+      control_third_var = {{ thirdVar }}
+    ) |>
+    select(-c({{ caseControl }}))
 
-  # Initialize usage counts for cases
-  case_usage <- rep(0, nrow(case))
-
-  # Define a function to check if there are any possible matches for a case
-  check_possible_matches <- function(idVar, age, gender, ethnicity, event, control) {
-    # Create a tibble for the case row
-    case_row <- tibble(
-      case_id = idVar,
-      age = age,
-      gender = gender,
-      ethnicity = ethnicity,
-      event = event
-    )
-
-    possible_controls <- control |>
-      filter(
-        ethnicity == case_row$ethnicity,
-        age >= case_row$age - numRange,
-        age <= case_row$age + numRange,
-        control_usage <= 1  # Only consider controls that have been used once or not at all
-      )
-
-    # Return TRUE if there are any possible controls, FALSE otherwise
-    has_match <- nrow(possible_controls) > 0
-    if (is.na(has_match)) {
-      FALSE
-    } else {
-      has_match
-    }
-  }
+  # set this to be an initial empty value
+  combined_rows <- NULL
 
   # Define a function to find two controls for each case and return a single row
   # that includes the case and its matched controls
-  find_controls <- function(idVar, age, gender, ethnicity, event) {
-    # Create a tibble for the case row
-    case_row <- tibble(
-      case_id = idVar,
-      age = age,
-      gender = gender,
-      ethnicity = ethnicity,
-      event = event
-    )
+  find_matches <- function(this_case) {
+
+    # Get the case row
+    case_row <- case |> filter(case_id == this_case)
+
+    # Access the numVar and catVar columns in the case_row data frame
+    numeric_value <- case_row |> pull(case_num_var)
+    categorical_value <- case_row |> pull(case_cat_var)
+    if (!is.null(thirdVar)) {
+      third_value <- case_row |> pull(case_third_var)
+    }
 
     # Check if there are any possible matches for this case
-    case_usage_count <- ifelse(!is.na(case_usage[case_row$case_id]), case_usage[case_row$case_id], 0)
-    if (check_possible_matches(idVar, age, gender, ethnicity, event, control) && case_usage_count < ratio) {
-      matched_controls <- control |>
-        filter(
-          ethnicity == case_row$ethnicity,
-          age >= case_row$age - numRange,
-          age <= case_row$age + numRange,
-          control_usage <= 1  # Only consider controls that have been used once or not at all
-        ) |>
-        mutate(random_num = runif(n(), 0, 1)) |>
-        arrange(random_num) |>
-        sample_n(size = min(n(), ratio)) |>
-        rename(
-          control_id = idVar,
-          control_age = age,
-          control_gender = gender,
-          control_ethnicity = ethnicity,
-          control_event = event
+    possible_controls <- control |>
+      filter(
+        control_cat_var == categorical_value,
+        control_num_var >= numeric_value - numRange,
+        control_num_var <= numeric_value + numRange,
+        control_third_var == third_value,
+        # Only consider controls that have not been used
+        !control_id %in% combined_rows$control_id,
+        control_usage == 0
+      )
+
+    # If there are no possible matches, return NULL
+    if (nrow(possible_controls) == 0) {
+      return(NULL)
+    }
+
+    # Select two controls at random
+    matched_controls <- possible_controls |>
+      slice_sample(n = ratio)
+
+    # Combine the case and its matched controls into a single row
+    combined_rows <<- bind_cols(case_row, matched_controls) |>
+      select(
+        case_id, case_num_var, case_cat_var, case_third_var,
+        control_id, control_num_var
+      )
+
+    # Update the control usage count
+    control <<- control |>
+      mutate(
+        control_usage = case_when(
+          control_id %in% combined_rows$control_id ~ 1,
+          TRUE ~ control_usage
         )
+      )
+    # browser()
 
-      # Update usage counts
-      case_usage[case_row$case_id] <<- case_usage[case_row$case_id] + 1
-      control <<- control |>
-        mutate(control_usage = ifelse(idVar %in% matched_controls$control_id, control_usage + 1, control_usage))
+    return(combined_rows)
+    # return(list(combined_rows = combined_rows, control = control))
+  } # end of find_matches()
 
-      # Remove the matched controls from the controls data frame
-      control <<- control |>
-        filter(!idVar %in% matched_controls$control_id)
+  # Apply the function
+  matched_data <- case$case_id |> map_dfr(find_matches)
 
-      # Combine the case and its matched controls into a single row
-      combined_row <- bind_cols(case_row, matched_controls)
-
-      combined_row
-    } else {
-      # If there are no possible matches, return NULL
-      NULL
-    }
-  }
-
-  # Initialize matched_data as an empty tibble
-  matched_data <- tibble()
-
-  # While there are still unmatched cases, try to find matches
-  while(nrow(case) > 0) {
-    # Try to find matches for each case
-    new_matches <- pmap_dfr(case, find_controls)
-
-    # If no new matches were found, break the loop
-    if(nrow(new_matches) == 0) {
-      break
-    }
-
-    # Add the new matches to the matched data
-    matched_data <- bind_rows(matched_data, new_matches)
-
-    # Remove the matched cases from the cases data frame
-    case <- case |>
-      filter(!idVar %in% new_matches$case_id)
-  }
-
-  # Remove NULL rows
-  matched_data <- matched_data[!is.na(matched_data$case_id), ]
-
-  matched_data
+  return(matched_data)
 }
 
-mitter_match_original(
-  dat = dat,
-  numRange = numRange,
-  ratio = ratio
+
+# use matching algorithm with loop
+do_matching <- function(dat, idVar, caseControl, numVar, numRange, catVar, ratio, thirdVar = NULL) {
+
+  # # Convert the variable names to symbols
+  # idVar <- rlang::sym(idVar)
+  # caseControl <- rlang::sym(caseControl)
+  # catVar <- rlang::sym(catVar)
+  # numVar <- rlang::sym(numVar)
+  # if (!is.null(thirdVar)) {
+  #   thirdVar <- rlang::sym(thirdVar)
+  # }
+
+  best_matched_data <- NULL
+  for(i in 1:3) {
+    print(glue::glue("Starting iteration {i}..."))
+    # get the results of the best iteration
+    if (!is.null(best_matched_data)) {
+      best_match_results <- best_matched_data |>
+        summarise(
+          n_cases = n_distinct(case_id),
+          n_controls = n_distinct(control_id)
+        )
+    }
+    # run the matching algorithm
+    matched_data <- mitter_match(
+      dat,
+      idVar,
+      caseControl,
+      numVar,
+      numRange,
+      catVar,
+      ratio,
+      thirdVar
+    )
+
+    # count the number of unique cases and controls
+    match_results <- matched_data |>
+      summarise(
+        n_cases = n_distinct(case_id),
+        n_controls = n_distinct(control_id)
+      )
+    # print the results
+    print(
+      glue::glue(
+        "Iteration {i} has {nrow(matched_data)} rows and {match_results$n_cases} used cases."
+      )
+    )
+
+    # compare the results to the best results
+    if (is.null(best_matched_data)) {
+      best_matched_data <- matched_data
+    } else {
+      if (match_results$n_cases > best_match_results$n_cases) {
+        best_matched_data <- matched_data
+      } else if (match_results$n_cases == best_match_results$n_cases &&
+                 nrow(matched_data) > nrow(best_matched_data)) {
+        best_matched_data <- matched_data
+      } else {
+        NULL
+      }
+    }
+  }
+
+  return(best_matched_data)
+}
+
+my_matches <- do_matching(
+  dat,
+  idVar,
+  caseControl,
+  numVar,
+  numRange,
+  catVar,
+  ratio,
+  thirdVar
 )
-
-
-
-
-
-
-
-
-

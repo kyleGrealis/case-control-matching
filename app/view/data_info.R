450702c860a78b2c0953bd5f73b703d9e5c70d9c
Kyle Grealis <kyleGrealis@icloud.com>
2023-12-29 13:16:48 -0600

    edit: formatting of num vars

--- app/view/data_info.R
+++ app/view/data_info.R
@@ -1,6 +1,7 @@
 #' @concept This script will display information about the selected dataset.
 
 box::use(
+  dplyr[across, mutate, where],
   glue[glue],
   shiny[moduleServer, NS, renderTable, renderUI, tableOutput, tagList, uiOutput],
   utils[head]
@@ -20,11 +21,26 @@ ui <- function(id) {
 server <- function(id, newFile) {
   moduleServer(id, function(input, output, server) {
 
+    # Custom function to format numbers -- was originally adding decimals to
+    # whole numbers. May need to be re-addressed
+    format_numbers <- function(x) {
+      if (all(x == floor(x))) {
+        return(format(x, nsmall = 0, scientific = FALSE))
+      } else {
+        return(x)
+      }
+    }
+
     output$contents <- renderTable({
       if (is.null(newFile())) {
         return(NULL)
       }
-      head(newFile(), n = 20)
+
+      # Apply the formatting to the numeric columns
+      df <- newFile() |>
+        mutate(across(where(is.numeric), format_numbers))
+
+      head(df, n = 20)
     })
 
     output$tableSummaryMessage <- renderUI({

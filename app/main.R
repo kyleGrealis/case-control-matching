7be2324758e27f79f406ca3efea3518d1dd520c0
Kyle Grealis <kyleGrealis@icloud.com>
2023-12-29 13:17:22 -0600

    feat: added inputs module

--- app/main.R
+++ app/main.R
@@ -4,6 +4,7 @@ box::use(
 )
 
 box::use(
+  app/logic/matching_algo,
   app/view/data,
   app/view/data_info,
   app/view/inputs,
@@ -15,7 +16,7 @@ ui <- function(id) {
   page_fixed(
     sidebarLayout(
       sidebarPanel(
-        data$ui(ns("file1")),
+        data$ui(ns("data_file")),
         inputs$ui(ns("inputs"))
       ),
       mainPanel(
@@ -29,7 +30,7 @@ ui <- function(id) {
 server <- function(id) {
   moduleServer(id, function(input, output, session) {
     ns <- NS(id)
-    newFile <- data$server("file1")
+    newFile <- data$server("data_file")
     data_info$server("info", newFile)
     inputs$server("inputs", newFile)
   })

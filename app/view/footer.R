57a6162691d290dad37e042c8279dcd476e34c3e
Kyle Grealis <kyleGrealis@icloud.com>
2024-01-05 12:30:40 -0600

    feat: adding app footer

--- app/view/footer.R
+++ app/view/footer.R
@@ -17,5 +17,5 @@ footer <- Stack(
     block=TRUE
   ),
   Text(variant = "medium", nowrap = FALSE, "All rights reserved ©2024"),
-  style = "position: fixed; width: 100%; left: 0; bottom: 0; color: black; padding: 2px 20px; border-top: 0.4px solid black;"
+  style = "position: fixed; width: 100%; left: 0; bottom: 0; color: black; padding: 2px 20px; border-top: 0.4px solid black; background-color: #f3f3f3;"
 )

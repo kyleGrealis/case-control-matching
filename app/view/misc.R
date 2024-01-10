1a27ca110d49770677b47eda9d4a7f8606601693
Kyle Grealis <kyleGrealis@icloud.com>
2024-01-09 18:21:22 -0600

    moved function to app/logic/functions

--- app/view/misc.R
+++
@@ -1,9 +0,0 @@
-#' This script is for miscellaneous, reusable components
-
-box::use(
-  bslib[tooltip],
-)
-#' @export
-my_tooltip <- function(x) {
-  tooltip(x, "Sort the table by clicking the variable names.", placement = "top")
-}

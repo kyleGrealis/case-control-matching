7a5193a4f570c4376687e17761a38e8143d4bb52
Kyle Grealis <kyleGrealis@icloud.com>
2024-01-10 17:40:57 -0600

    feat: using {reactable} and upgraded user output

--- app/view/matched_results.R
+++ app/view/matched_results.R
@@ -4,10 +4,11 @@
 #' while the unmatched cases & controls will go to their respective tabs.
 
 box::use(
-  dplyr[across, mutate, where],
+  dplyr[across, filter, mutate, n_distinct, summarize, where],
   glue[glue],
   reactable[reactable, reactableOutput, renderReactable],
-  shiny[moduleServer, NS, renderUI, req, tagList, uiOutput],
+  rlang[sym],
+  shiny[moduleServer, NS, reactive, renderUI, req, tagList, uiOutput],
 )
 
 # MUST use renderUI & uiOutput in order to use the tooltip with glue!
@@ -26,8 +27,30 @@ ui <- function(id) {
 }
 
 #' @export
-server <- function(id, results) {
+server <- function(id, newFile, inputs, results) {
   moduleServer(id, function(input, output, session) {
+
+    # reusing this from the app/view/algo module to calculate the number of cases
+    # find total number of cases in users dataset
+    total_cases <- reactive({
+      req(newFile(), inputs())
+      # browser()
+      newFile <- newFile()
+      caseControl <- rlang::sym(inputs()$caseControl)
+      newFile |>
+        filter({{ caseControl }} == 1) |>
+        nrow()
+    })
+
+    cases_used <- reactive({
+      req(results())
+      results <- results()
+      results |>
+        summarize(
+          n_cases = n_distinct(case_id),
+          n_controls = n_distinct(control_id)
+        )
+    })
 
     output$instructions <- renderUI({
       req(results())
@@ -35,7 +58,7 @@ server <- function(id, results) {
         return(NULL)
       }
       glue::glue(
-        "There are {nrow(results())} successful matches."
+        "{cases_used()$n_cases} of {total_cases()} total cases successfully matched to {nrow(results())} controls."
       ) |>
         my_tooltip()
     })

bbe633b53ed20466870f4a7aafa7b356f27af06a
Kyle Grealis <kylegrealis@icloud.com>
2025-04-14 14:47:37 -0500

    updated for 2025

--- app/view/footer.R
+++ app/view/footer.R
@@ -16,6 +16,6 @@ footer <- Stack(
     ),
     block=TRUE
   ),
-  Text(variant = "medium", nowrap = FALSE, "All rights reserved ©2024"),
+  Text(variant = "medium", nowrap = FALSE, "All rights reserved ©2025"),
   style = "position: fixed; width: 100%; left: 0; bottom: 0; color: black; padding: 2px 20px; border-top: 0.4px solid black; background-color: #f3f3f3;"
 )
